b01a2ade53465c8405c79207d9c9bc91e02fa22f
Selby Kendrick <selby.kendrick@gmail.com>
2014-10-29 19:21:51 -0500

    db yet?

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
 var compress = require('compression');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(compress());
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
