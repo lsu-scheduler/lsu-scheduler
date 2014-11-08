5ca7ad444a158bff22ca13ef079a7acb4d5339ed
Selby Kendrick <selby.kendrick@gmail.com>
2014-11-08 00:47:06 -0600

    trick just for fun

--- app/components/schedule-component.js
+++ app/components/schedule-component.js
@@ -16,6 +16,8 @@ export default Ember.Component.extend({
     }).on("didInsertElement"),
     actions: {
       addEvent: function() {
+        alert('Gotcha.\n\nHead over to GitHub and make it work.');
+        window.location.href = "https://github.com/selbyk/lsu-scheduler";
         var newEvent = {title: "New Event!", start: "2014-06-27T05:00:00", allDay: false};
         this.theEvents.pushObject(newEvent);
         this.$("#schedule").fullCalendar('renderEvent', newEvent, true);
