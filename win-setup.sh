92536b802fcb8161606737514f3cf5d945c34c28
Selby Kendrick <selby.kendrick@gmail.com>
2014-11-13 19:37:28 -0600

    eh

---
+++ win-setup.sh
@@ -0,0 +1,8 @@
+﻿#
+echo 'These are the commands it takes to get windows set up'
+export PATH=$PATH:./
+npm.cmd install -g ember-cli 
+npm.cmd install -g bower 
+npm.cmd install
+bower.cmd install
+
