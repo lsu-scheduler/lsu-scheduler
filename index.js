5b66f614a86c8ae853b1dfb57f7e0715eb78cc4a
chester357 <chester35711@gmail.com>
2014-11-16 13:59:37 -0600

    Commiting deletions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
-      if (err)
+      if(err)
        { console.error(err); response.send("Error " + err); }
       else
        { response.send(result.rows); }
