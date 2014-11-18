040fb168c59c41a792c6a2f5fd8dbee0d7b85423
Selby Kendrick <selby.kendrick@gmail.com>
2014-11-17 22:11:11 -0600

    Docs

---
+++ dist/docs/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Application Course Model",
+        "Application Department Model",
+        "Application Instructor Model",
+        "Application Schedule Model",
+        "LSU Scheduler API",
+        "Sequelize Course Model",
+        "Sequelize Department Model",
+        "Sequelize Instructor Model"
+    ],
+    "modules": [
+        "lsu-scheduler"
+    ],
+    "allModules": [
+        {
+            "displayName": "lsu-scheduler",
+            "name": "lsu-scheduler",
+            "description": "Server script that controls the LSU Scheduler backend, including\nthe API used to communicate with front-end clients, managing data requests,\nand fetching updated information from LSU's system."
+        }
+    ]
+} };
+});
