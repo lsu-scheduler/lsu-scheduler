3c6db551f5e2c1a503d481f99686074b154b1272
chester357 <chester35711@gmail.com>
2014-11-17 21:31:27 -0600

    Generated Docs

--- app/routes/course.js
+++ app/routes/course.js
@@ -1,9 +1,26 @@
 import Ember from 'ember';
 
+//========================================================================================
+/*
+/* FILE NAME: App/Route/Course.js
+/*
+/* DESCRIPTION: Resposible for connecting the front end to the back end
+/*
+/* AUTHORS: Chester Schofield
+/*
+/* REFERENCE:
+/*
+/* DATE BY CHANGE DESCRIPTION
+/* ======== ======= ===========
+/* 11/17/2014 Chester Added base comments
+/*
+*/
+//========================================================================================
+
+/**
+ * Connects the Application Course to the Database Course
+ */
 export default Ember.Route.extend({
-
-	// What does this do?
-
 	model: function (){
     	return this.store.find('course');
   	}
--- app/routes/department.js
+++ app/routes/department.js
@@ -1,5 +1,25 @@
 import Ember from 'ember';
 
+//========================================================================================
+/*
+/* FILE NAME: App/Route/Department.js
+/*
+/* DESCRIPTION: Resposible for connecting the front end to the back end
+/*
+/* AUTHORS: Chester Schofield
+/*
+/* REFERENCE:
+/*
+/* DATE BY CHANGE DESCRIPTION
+/* ======== ======= ===========
+/* 11/17/2014 Chester Added base comments
+/*
+*/
+//========================================================================================
+
+/**
+ * Connects the Application Department to the Database Department
+ */
 export default Ember.Route.extend({
   model: function(){
     return this.store.find('department');
